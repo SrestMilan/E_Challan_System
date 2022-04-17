10210669a137d8784b3c7c7344477a5bd325cfe6
Milan <shrestham780@gmail.com>
2022-04-17 13:41:26 +0545

    chnage in css files of login

--- src/main/java/com/system/ChallanController.java
+++ src/main/java/com/system/ChallanController.java
@@ -4,14 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 
 
 @Controller
@@ -20,19 +14,12 @@ public class ChallanController {
     @Autowired(required = true)
     private ChallanService challanRepo;
 
-
-
-
-
     @GetMapping("/")
     public String login(Model model){
         User user=new User();
         model.addAttribute("user",user);
         return "login";
     }
-
-
-
 
     // display list of employees
     @GetMapping("/viewHomePage")
--- src/main/java/com/system/User.java
+++ src/main/java/com/system/User.java
@@ -9,6 +9,30 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Column(name = "userId")
     private String userId;
 
